3bd51d8ccd3016e45a6f8480ad924d9dd2cc5d7a
北途 <461053472@qq.com>
2018-07-04 10:27:11 +0800

    Update db_config.js

--- api/config/db_config.js
+++ api/config/db_config.js
@@ -2,7 +2,7 @@ module.exports = {
     dev_db: {
         host: "127.0.0.1",
         user: "root",
-        password: "638799swb.520",
+        password: "638799",
         port: 3306,
         database: "blog_sql"
     }
